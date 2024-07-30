f9761a7f25d8a51ccdafb49ef1749480ca20553e
katyadenchik <katyadenchik11@gmail.com>
2024-07-30 13:52:34 +0300

    create project

---
+++ VitaminStoreBot/VitaminStoreBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VitaminStoreBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
